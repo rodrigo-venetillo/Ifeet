43ccc9147fdebeaee0b8c4c0992bfcb20dddb6a5
rhuanlucass <rhuanfernandes.rf@gmail.com>
2024-04-11 18:15:32 -0300

    Adicionando validação de senhas a página registrar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,53 @@
-(() => {
-  'use strict'
+// (() => {
+//   'use strict'
+// // const forms = document.querySelectorAll('.needs-validation');
+// const password = document.getElementById('password');
+// const confirmPassword = document.getElementById('confirm-password');
+// const confirm = document.getElementById('confirm');
+// const passw = document.getElementById('passw');
 
-  const forms = document.querySelectorAll('.needs-validation')
+// function validationLength(event) {
+//   if(password.value.length > 0)
+//   if(password.value.length < 8){
+//     event.preventDefault()
+//     event.stopPropagation()
+//     passw.innerText = 'A senha deve ter pelo menos 8 dígitos!';
+//     passw.style.display = 'block';
+//     password.classList.add('confirm-pass');
+//   }
+//   else{
+//     passw.style.display = 'none';
+//     password.classList.remove('confirm-pass');
+//   }
+  
+//   if(confirmPassword.value.length < 8){
+//     event.preventDefault()
+//     event.stopPropagation()
+//     confirmPassword.classList.add('confirm-pass');
+//   }
+//   else{
+//     confirm.style.display = 'none';
+//     confirmPassword.classList.remove('confirm-pass');
+//   }
+//   form.classList.add('was-validated')
+// }
 
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+// function validationConfirm(event) {
+//   if(password.value !== confirmPassword.value || password.value === ''){
+//     event.preventDefault()
+//     event.stopPropagation()
+//     confirm.innerText = 'As senhas não conferem!';
+//     confirm.style.display = 'block';
+//     confirmPassword.classList.add('confirm-pass');
+//     password.classList.add('confirm-pass');
+//   }
+//   else{
+//     confirm.style.display = 'none';
+//     passw.style.display = 'none';
+//     confirmPassword.classList.remove('confirm-pass');
+//     password.classList.remove('confirm-pass');
+//   }
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+//   form.classList.add('was-validated')
+// }
+// })()
